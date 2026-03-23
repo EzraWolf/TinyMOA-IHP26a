e8c4c3b80c1d7fccfa03598167729a5e2257d5b3
Ezra Wolf <wolfez@mail.uc.edu>
2026-03-23 15:17:36 -0400

    Fix pinout

--- src/project.v
+++ src/project.v
@@ -10,20 +10,22 @@ module tt_um_tinymoa_ihp26a (
     input  wire       clk,
     input  wire       rst_n,
     input  wire       ena, // always high, can ignore.
-
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
-    input  wire [7:0] uio_io,
+    inout  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
     output wire [7:0] uio_oe
 );
+
     tinymoa_top top (
-      .clk    (clk),                                                                                                                                                                                                                                                
-      .nrst   (rst_n),
-      .ena    (ena),                                                                                                                                                                                                                                                
-      .ui_in  (ui_in),                                      
-      .uo_out (uo_out),                                                                                                                                                                                                                                             
-      .uio_io (uio_io),
-      .uio_oe (uio_oe)                                                                                                                                                                                                                                              
+      .clk     (clk),                                                                                                                                                                                                                                                
+      .nrst    (rst_n),
+      .ena     (ena),                                                                                                                                                                                                                                                
+      .ui_in   (ui_in),                                      
+      .uo_out  (uo_out),                                                                                                                                                                                                                                             
+      .uio_in  (uio_in),
+      .uio_out (uio_out),
+      .uio_oe  (uio_oe)                                                                                                                                                                                                                                              
     );       
 
     wire _unused = ena;
